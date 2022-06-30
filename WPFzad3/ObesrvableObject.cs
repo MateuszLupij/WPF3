86984b8e60f04bcc408c2c8d31f9bf3e50b7e69f
Mateusz Łupij <mateuszlupij@gmail.com>
2022-06-30 15:32:08 +0200

    WPF zadanie 3

---
+++ WPFzad3/ObesrvableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace WPFzad3
+{
+    public class ObesrvableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
